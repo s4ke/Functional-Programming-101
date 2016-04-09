14e182ce8d8ef2d7a13ca630c8780fa90318a9ad
Martin Braun <martinbraun123@aol.com>
2016-04-09 15:39:09 +0200

    Monad implementation for the Tree.

--- learnyouahaskell/MyTree.hs
+++ learnyouahaskell/MyTree.hs
@@ -5,19 +5,15 @@
 module Main where
 
 import Prelude
-import qualified Data.Foldable as F 
-import Control.Applicative 
+import qualified Data.Foldable as F
 
 testTree :: Tree Integer
 testTree = Node 5  
-            (Node 3  
-                (Node 1 Empty Empty)  
-                (Node 6 Empty Empty)  
-            )  
-            (Node 9  
-                (Node 8 Empty Empty)  
-                (Node 10 Empty Empty)  
+            (Node 3  Empty (
+                        Node 9 Empty (Node 8 Empty Empty)
+                    )
             )
+            (Node 10 Empty Empty)
 
 fnTree = Node (+5)
             (Node (*3)  
@@ -47,7 +43,17 @@ instance Applicative Tree where
     Empty <*> _ = Empty
     _ <*> Empty = Empty
     Node f left1 right1 <*> Node x left2 right2 =
-        Node (f x) (left1 <*> left2) (right1 <*> right2)
+        Node (f x) (left1 <*> left2) (right1 <*> right2)  
+
+instance Monad Tree where
+    return x = Node x Empty Empty
+    Empty >>= f = Empty
+    (Node x left right) >>= f = 
+        let (Node y leftY rightY) = f x
+        -- to get the correct in-order traversion
+        -- all new left elements are used as smaller than the old left ones
+        -- and all new right elements are used as greater than the old right ones
+        in Node y (leftY `mappend` (left >>= f)) ((right >>= f) `mappend` rightY)
         
 instance Monoid (Tree a) where
     mempty = Empty
@@ -61,8 +67,25 @@ instance Monoid (Tree a) where
 printAsList tree = putStrLn $ show $ F.foldMap (pure :: a -> [a]) tree
         
 main = do
+    printAsList testTree
     putStrLn $ show $ F.foldMap (pure :: a -> [a]) (fmap (+5) testTree)
     putStrLn $ show $ F.foldMap (pure :: a -> [a]) (fnTree <*> testTree)
     putStrLn $ show $ F.foldl (+) 0 $ F.foldMap (pure :: a -> [a]) (fnTree <*> testTree)
     putStrLn $ show $ F.foldl (+) 0 (fnTree <*> testTree)
     printAsList $ testTree `mappend` testTree
+    printAsList $ (*) <$> testTree <*> testTree
+    putStrLn ""
+    
+    putStrLn "return x >>= f Monad law"
+    print $ return 2 >>= (\x -> Node x Empty Empty)
+    print $ (\x -> Node x Empty Empty) 2
+    putStrLn ""
+    
+    putStrLn "m >>= return = m Monad law"
+    print $ testTree
+    print $ testTree >>= return
+    putStrLn ""
+    
+    putStrLn "(m >>= f) >>= g = m >>= (\\x -> f x >>= g)"
+    print $ testTree  >>= (\x -> Node (x + 1) Empty Empty) >>= (\x -> Node (x * 2) Empty Empty)
+    print $ testTree  >>= (\x -> (Node (x + 1) Empty Empty) >>= (\x -> Node (x * 2) Empty Empty))
