c47264dac3bb33f7c5c4e531b0fed6122e24f360
Martin Braun <martinbraun123@aol.com>
2016-04-10 20:03:51 +0200

    Fibonacci added

---
+++ random-stuff/fib.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Prelude
+import System.Environment
+
+fib :: Int -> Int
+fib 0 = 1
+fib 1 = 1
+fib x = (fib (x - 1)) + (fib (x - 2))
+
+main = do
+    fmap (fib . read . head) getArgs >>= print
